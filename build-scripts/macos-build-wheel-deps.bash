feb0d87960de7927f6624fddb112c6b43cca2a4f
James R. Barlow <james@purplerock.ca>
2021-06-07 02:50:41 -0700

    patch ws

--- build-scripts/macos-build-wheel-deps.bash
+++ build-scripts/macos-build-wheel-deps.bash
@@ -7,13 +7,13 @@ if [ ! -f /usr/local/lib/libz.a ]; then
 --- zlib-1.2.11/configure	2016-12-31 10:06:40.000000000 -0800
 +++ zlib/configure	2021-05-31 01:13:39.000000000 -0700
 @@ -186,6 +186,7 @@
-   SFLAGS="${CFLAGS--O3} -fPIC"
-   if test "$ARCHS"; then
-     CFLAGS="${CFLAGS} ${ARCHS}"
-+    SFLAGS="${SFLAGS} ${ARCHS}"
-     LDFLAGS="${LDFLAGS} ${ARCHS}"
-   fi
-   if test $build64 -eq 1; then
+   SFLAGS="${CFLAGS--O3} -fPIC"
+   if test "$ARCHS"; then
+     CFLAGS="${CFLAGS} ${ARCHS}"
++    SFLAGS="${SFLAGS} ${ARCHS}"
+     LDFLAGS="${LDFLAGS} ${ARCHS}"
+   fi
+   if test $build64 -eq 1; then
 EOF
     ./configure --archs="-arch x86_64 -arch arm64" && make -j install
     popd
